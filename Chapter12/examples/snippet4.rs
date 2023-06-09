ac4631075c66571229045e6b00182311103ef31a
thinkgos <thinkgo@aliyun.com>
2023-06-09 19:36:54 +0800

    fix: add chapter12

---
+++ Chapter12/examples/snippet4.rs
@@ -0,0 +1,17 @@
+use std::env::var;
+
+static mut THREAD_COUNT: u32 = 4;
+
+fn change_thread_count(count: u32) {
+    unsafe {
+        THREAD_COUNT = count;
+    }
+}
+fn main() {
+    if let Some(thread_count) = var("THREAD_COUNT").ok() {
+        change_thread_count(thread_count.parse::<u32>().unwrap());
+    };
+    unsafe {
+        println!("Thread count is: {}", THREAD_COUNT);
+    }
+}
